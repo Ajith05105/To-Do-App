32ecd8b2ae4ea9c365b5df40985fbed3466a6e39
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2025-02-20 01:54:19 +1300

    able to temp add pointless task boxs

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const button = document.getElementById('click');
-button.addEventListener('click', () => {
-  alert('Button was clicked!');
+const addBoxButton = document.getElementById('add-box');
+const boxContainer = document.getElementById('task-boxs');
+addBoxButton.addEventListener('click', () => {
+  alert('Button clicked');
+  boxContainer.innerHTML += ` <div class = "box">Tasks: 
+            <li>Task 1</li>
+            <li>Task 2</li>
+            <li>Task 3</li>
+            <button id = "add-task">+ Add Task</button>
+        </div>`;
 });
