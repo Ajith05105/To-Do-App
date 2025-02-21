5e047e13f309a70e9465837bacb79630ab47eb50
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2025-02-21 20:06:40 +1300

    made the add task button work for testing purpose, however the task list box does not stretch down with the task list growing

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const addBoxButton = document.getElementById('add-box');
 const boxContainer = document.getElementById('task-boxs');
+const addTaskButton = document.getElementById('add-task');
+const taskList = document.getElementById('task-list');
+
+addTaskButton.addEventListener('click', () => {
+  alert('Button clicked');
+  taskList.innerHTML += `<li>Task 4</li>`;
+});
+
 addBoxButton.addEventListener('click', () => {
   alert('Button clicked');
   boxContainer.innerHTML += ` <div class = "box">Tasks: 
+              <span id = "task-list">
             <li>Task 1</li>
             <li>Task 2</li>
             <li>Task 3</li>
+            </span>
             <button id = "add-task">+ Add Task</button>
         </div>`;
-});
+}); 
